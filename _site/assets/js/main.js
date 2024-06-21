36bc5ac44548ff02e8cb16769c19f0b1304307d6
RajuKalidindi <krkraju997@gmail.com>
2024-06-21 17:12:51 +0100

    Add project section with 2 projects

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -4,7 +4,7 @@ document.addEventListener(
     new SweetScroll({});
     particlesJS("particles-js", {
       particles: {
-        number: { value: 50, density: { enable: !0, value_area: 800 } },
+        number: { value: 150, density: { enable: !0, value_area: 800 } },
         color: { value: "#ffffff" },
         shape: {
           type: "polygon",
